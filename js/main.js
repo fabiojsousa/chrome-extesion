949a21fc97750b94a1689c070bd6f6b9dff8888c
Fábio Sousa <fabiojdev@gmail.com>
2020-03-06 00:13:34 -0300

    Countries buttons

--- js/main.js
+++ js/main.js
@@ -1,17 +1,59 @@
-document.addEventListener('click', ()=>{
-    // window.open(url, '_blank')
-})
+const domainElement = document.querySelector("#domain");
+const countryElement = document.querySelector("#country");
+const urlElement = document.querySelector("#url");
 
-const country = document.querySelector('#country').value;
+const baseUrl = `https://www.google.com/shopping/ratings/account/metrics?q=`;
 
+const countryLocalStorage = localStorage.getItem("countryValue");
+
+let country = countryLocalStorage ? countryLocalStorage : "None";
 
 const url = document.location.href;
-// const url = 'https://www.teste.asdfasdf.facebook.com';
 
-document.querySelector('#url').url;
-
-const regex = /\w*\.(com|net|info|org)/gmi;
+const regex = /\w*\.(com|net|info|org)/gim;
 
 const domain = url.match(regex);
 
-console.log(domain);
+domainElement.innerText = domain;
+
+let targetUrl = `${baseUrl}${domain}`;
+
+targetUrl = country !== "" ? targetUrl + `&c=${country}&v=4` : targetUrl;
+
+urlElement.innerText = targetUrl;
+
+function openSearchUrl() {
+  window.open(targetUrl, "_blank");
+}
+
+function setCountry(e) {
+  country = e.target.innerText;
+  localStorage.setItem("countryValue", country);
+
+  targetUrl =
+    country !== "None"
+      ? baseUrl + domain + `&c=${country}&v=4`
+      : baseUrl + domain;
+
+  urlElement.innerText = targetUrl;
+
+  setCountryCss();
+}
+
+function setCountryCss() {
+  const countryItems = document.querySelectorAll(".country-item");
+
+  countryItems.forEach(e => {
+    if (country === e.innerText) {
+      e.style["background-color"] = "#2F84D7";
+      e.style["border"] = "1px solid black";
+      e.style["color"] = "#fff";
+    } else {
+      e.style["backgroundColor"] = "";
+      e.style["border"] = "1px solid burlywood";
+      e.style["color"] = "";
+    }
+  });
+}
+
+setCountryCss();
